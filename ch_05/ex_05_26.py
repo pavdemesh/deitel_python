949ccab9c701551f075093ccebd0f586d09045ae
pavdemesh <pavdemesh@gmail.com>
2021-04-19 16:11:06 +0300

    done

--- ch_05/ex_05_26.py
+++ ch_05/ex_05_26.py
@@ -1,8 +1,8 @@
 """
-Modify Exercise 5.24 to deal a five-card poker hand as a list of five card tuples.
-Then create functions (i.e., is_pair, is_two_pair, is_three_of_a_kind, …)
-that determine whether the hand they receive as an argument contains groups of cards, such as one pair etc.
-Assign numerical value to hand based on cards they receive
+Use the methods developed in Exercise 5.25 to:
+Write a script that deals two five-card poker hands (i.e., two lists of five-card tuples each),
+evaluates each hand and determines which wins.
+As each card is dealt, it should be removed from the list of tuples representing the deck.
 """
 import random
 import numpy as np
@@ -58,7 +58,7 @@ def is_one_pair_basic(hand):
             faces_w_freqs.append([face, faces.count(face)])
 
     # Check that only one frequency is 2 and no frequency is 3 present
-    if 2 in [z[1] for z in faces_w_freqs] and [z[1] for z in faces_w_freqs].count(2) == 1\
+    if 2 in [z[1] for z in faces_w_freqs] and [z[1] for z in faces_w_freqs].count(2) == 1 \
             and 3 not in [z[1] for z in faces_w_freqs]:
         pair_to_print = list(filter(lambda x: x[1] == 2, faces_w_freqs))
         print(f"You have one pair of {pair_to_print[0][0]}s")
@@ -84,13 +84,11 @@ def is_one_pair_numpy(hand):
     # Check that only one frequency is 2 and no frequency is 3 present
     if 2 in frequencies and list(frequencies).count(2) == 1 and 3 not in frequencies:
         pair_index = list(frequencies).index(2)
-        print(f"You have one pair of {faces[pair_index]}s")
+        print(f"You have one pair of {faces[pair_index]}s:")
         print(*hand)
         return True
 
     # If not exactly one pair: print corresponding message and display hand as proof
-    print("You do not have exactly one pair. Your hand is:")
-    print(*hand)
     return False
 
 
@@ -103,20 +101,16 @@ def is_two_pair(hand):
     """
     # Generate 2 ndarrays holding faces and heir corresponding frequencies
     faces, frequencies = np.unique(list(x[0] for x in hand), return_counts=True)
+    faces, frequencies = list(faces), list(frequencies)
 
     # Check that only one frequency is 2 and no frequency is 3 present
-    if 2 in frequencies and list(frequencies).count(2) == 2:
-        print("You have two pair: ")
-        for pair_index, count in enumerate(list(frequencies)):
-            if count == 2:
-                print(f"One pair of {faces[pair_index]}s")
-        print("Your hand is:")
+    if 2 in frequencies and frequencies.count(2) == 2:
+        faces.pop(frequencies.index(1))
+        print(f"You have two pair: One pair of {faces[0]}s and one pair of {faces[1]}s:")
         print(*hand)
         return True
 
     # If not exactly two pair: print corresponding message and display hand as proof
-    print("You do not have exactly two pair. Your hand is:")
-    print(*hand)
     return False
 
 
@@ -133,14 +127,11 @@ def is_three_of_a_kind(hand):
     # Check that only one frequency is 2 and no frequency is 3 present
     if 3 in frequencies and 2 not in frequencies:
         print("You have three of a kind: ", end="")
-        print(f"Three {faces[list(frequencies).index(3)]}s")
-        print("Your hand is:")
+        print(f"Three {faces[list(frequencies).index(3)]}s:")
         print(*hand)
         return True
 
     # If not exactly three of a kind: print corresponding message and display hand as proof
-    print("You do not have three of a kind. Your hand is:")
-    print(*hand)
     return False
 
 
@@ -157,14 +148,11 @@ def is_four_of_a_kind(hand):
     # Check that only one frequency is 2 and no frequency is 3 present
     if 4 in frequencies:
         print("You have four of a kind: ", end="")
-        print(f"Four {faces[list(frequencies).index(4)]}s")
-        print("Your hand is:")
+        print(f"Four {faces[list(frequencies).index(4)]}s:")
         print(*hand)
         return True
 
     # If not exactly four of a kind: print corresponding message and display hand as proof
-    print("You do not have four of a kind. Your hand is:")
-    print(*hand)
     return False
 
 
@@ -181,14 +169,11 @@ def is_full_house(hand):
     # Check that only one frequency is 2 and no frequency is 3 present
     if 3 in frequencies and 2 in frequencies:
         print("You have a full-house: ", end="")
-        print(f"Three {faces[list(frequencies).index(3)]}s and Two {faces[list(frequencies).index(2)]}s")
-        print("Your hand is:")
+        print(f"Three {faces[list(frequencies).index(3)]}s and Two {faces[list(frequencies).index(2)]}s:")
         print(*hand)
         return True
 
     # If not exactly full-house: print corresponding message and display hand as proof
-    print("You do not have full-house. Your hand is:")
-    print(*hand)
     return False
 
 
@@ -220,33 +205,25 @@ def is_straight(hand):
     faces, faces_freqs = np.unique(list(x[0] for x in hand), return_counts=True)
     # Check that all faces are different == there are 5 faces
     if len(faces) != 5:
-        print("You do not have a straight. Your hand is:")
-        print(*hand)
         return False
 
     # If there are five different faces: check that they are not of same suit
     if 5 in suits_freqs:
-        print("You do not have a straight. Your hand is:")
-        print(*hand)
         return False
 
     if "Ace" not in faces:
         numerical_faces_ascending = sorted(list(map(lambda x: faces_to_nums_ace_one[x], faces)))
         if not is_ascending_by_one(numerical_faces_ascending):
-            print("You do not have a straight. Your hand is:")
-            print(*hand)
             return False
 
-    else:   # if Ace in faces
+    else:  # if Ace in faces
         num_faces_ascend_ace_one = sorted(list(map(lambda x: faces_to_nums_ace_one[x], faces)))
         num_faces_ascend_ace_fourteen = sorted(list(map(lambda x: faces_to_nums_ace_fourteen[x], faces)))
 
         if not (is_ascending_by_one(num_faces_ascend_ace_one) or is_ascending_by_one(num_faces_ascend_ace_fourteen)):
-            print("You do not have a straight. Your hand is:")
-            print(*hand)
             return False
 
-    print("You have a straight. Your hand is:")
+    print("You have a straight:")
     print(*hand)
     return True
 
@@ -264,14 +241,11 @@ def is_flush(hand):
     # Check that only one frequency is 2 and no frequency is 3 present
     if 5 in frequencies and not is_straight(hand):
         print("You have a flush: ", end="")
-        print(f"Five cards of {suits[0]} suit")
-        print("Your hand is:")
+        print(f"Five cards of {suits[0]} suit: ")
         print(*hand)
         return True
 
     # If not exactly flush: print corresponding message and display hand as proof
-    print("You do not have a flush. Your hand is:")
-    print(*hand)
     return False
 
 
@@ -289,17 +263,13 @@ def is_straight_flush(hand):
     faces, faces_freqs = np.unique(list(x[0] for x in hand), return_counts=True)
 
     if 5 not in suits_freqs:
-        print("You do not have a straight flush. Your hand is:")
-        print(*hand)
         return False
 
     num_faces_ascend = sorted(list(map(lambda x: faces_to_nums_ace_one[x], faces)))
     if not is_ascending_by_one(num_faces_ascend):
-        print("You do not have a straight flush. Your hand is:")
-        print(*hand)
         return False
 
-    print("You have a straight flush. You hand is:")
+    print("You have a straight flush:")
     print(*hand)
     return True
 
@@ -315,22 +285,72 @@ def is_royal_flush(hand):
     faces, faces_freqs = np.unique(list(x[0] for x in hand), return_counts=True)
 
     if 5 not in suits_freqs:
-        print("You do not have royal flush. Your hand is: ")
-        print(*hand)
         return False
 
     if any([True for x in faces if x not in ("Ace", "King", "Queen", "Jack", "Ten")]):
-        print("You do not have royal flush. Your hand is: ")
-        print(*hand)
         return False
 
-    print("You have a royal flush. Your hand is:")
+    print("You have a royal flush:")
     print(*hand)
     return False
 
 
+strength_combination_map = {1: is_one_pair_numpy, 2: is_two_pair, 3: is_three_of_a_kind, 4: is_straight, 5: is_flush,
+                            6: is_full_house, 7: is_four_of_a_kind, 8: is_straight_flush, 9: is_royal_flush}
+
+
+def determine_hand_strength(hand: list, values_combs: dict = strength_combination_map):
+    """
+    Determines hand strength (value) from 0 (no combination) to 9 (royal flush)
+    :param hand: A list of 5 tuples, each tuple represents "Face" and "Suit"
+    :param values_combs: Dictionary mapping integer value to corresponding function names determining combinations.
+    :return: Integer value which corresponds to combination or 0 if no combination could be found.
+    """
+    hand_value = 0
+    # Iterate over function combinations. If any returns True: return corresponding hand strength int
+    for strength, function in values_combs.items():
+        if function(hand):
+            print(f"This hand got you {strength} points.")
+            hand_value = strength
+            return hand_value
+    print("You have no valid combination:")
+    print(*hand)
+    print("This hand got you 0 points.")
+    return hand_value
+
+
+def compare_two_hands(hand1: list, hand2: list):
+    """
+    Compares the strength of two hands and prints out the winner
+    :param hand1: A list of 5 tuples, each tuple represents "Face" and "Suit"
+    :param hand2: A list of 5 tuples, each tuple represents "Face" and "Suit"
+    :return: Prints who wins: hand1 or hand2
+    """
+    print()
+    print("Determining the strength of Player 1's hand:...")
+    score_player_1 = determine_hand_strength(hand1)
+
+    print("-------------------------")
+
+    print("Determining the strength of Player 2's hand:...")
+    score_player_2 = determine_hand_strength(hand2)
+
+    print("-------------------------")
+    if score_player_1 > score_player_2:
+        print(f"Player 1 wins by having {score_player_1} point(s) against {score_player_2} point(s).")
+    elif score_player_2 > score_player_1:
+        print(f"Player 2 wins by having {score_player_2} point(s) against {score_player_1} point(s).")
+    else:
+        print(f"It's a draw, since both players have {score_player_2} point(s).")
+    return
+
+
+strength_combination_map = {1: is_one_pair_numpy, 2: is_two_pair, 3: is_three_of_a_kind, 4: is_straight, 5: is_flush,
+                            6: is_full_house, 7: is_four_of_a_kind, 8: is_straight_flush, 9: is_royal_flush}
+
 deck_of_cards = initialize_deck()
-dealt_hand = deal_hand(deck_of_cards)
 
-is_royal_flush(dealt_hand)
+dealt_hand_player_1 = deal_hand(deck_of_cards)
+dealt_hand_player_2 = deal_hand(deck_of_cards)
 
+compare_two_hands(dealt_hand_player_1, dealt_hand_player_2)
